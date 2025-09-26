1e038c1eea80478a4410580b4a806912c40ef8dc
EBB2675 <esmaboydas4@gmail.com>
2025-09-26 09:54:48 +0200

    rm redundant files

--- main.py
+++
@@ -1,7 +0,0 @@
-"""Simple entry point for the LLM Gradio app."""
-
-from nomad_ragbot import create_gradio_app
-
-if __name__ == "__main__":
-    app = create_gradio_app()
-    app.launch(share=False)
